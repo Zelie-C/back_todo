e2f25a91184b8372c95ca332294c1426701f4dfd
Zelie-C <zelie.chemin@gmail.com>
2023-09-08 14:16:43 +0200

    connexion sequelize ok

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,42 @@
 import 'dotenv/config'
 import express from 'express'
+import { Sequelize, DataTypes } from 'sequelize';
 
 let app = express();
 const port = parseInt(process.env.PORT as string);
+const database = process.env.DATABASE as string;
+const username = process.env.USERNAME as string;
+const password = process.env.PASSWORD as string;
+const server = process.env.SERVER as string;
+
+const sequelize = new Sequelize(database, username, password, {
+    dialect: 'mssql',
+    host: server,
+    dialectOptions: {
+      // Observe the need for this nested `options` field for MSSQL
+      options: {
+        // Your tedious options here
+        useUTC: false,
+        dateFirst: 1
+      }
+    }
+});
+
+async function connexionTest() {
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+connexionTest();
+
+const Todo = sequelize.define('Todo', {
+    
+})
+
 
 app.get('/toto', function (req, res) {
     res.send('toto')
