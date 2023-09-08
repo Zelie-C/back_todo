93473a37eadd8f0ad2894db07d0aee209120eefe
Zelie-C <zelie.chemin@gmail.com>
2023-09-08 12:15:25 +0200

    api fonctionnelle

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,13 @@ import express from 'express'
 let app = express();
 const port = parseInt(process.env.PORT as string);
 
-app.get(('/toto'), function (req, res) {
+app.get('/toto', function (req, res) {
     res.send('toto')
+})
+
+app.get('/test/:value', function (req, res) {
+    let taskValue: string = req.params.value;
+    res.send(`test ${taskValue}`)
 })
 
 app.listen(port, () => {
