fa1b45ba615b66f96208e639f65571a90e3aa7e0
Zelie-C <zelie.chemin@gmail.com>
2023-09-08 10:00:36 +0200

    fin initialisation back

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world')
+import 'dotenv/config'
+import express from 'express'
+
+let app = express();
+const port = parseInt(process.env.PORT as string);
+
+app.get(('/toto'), function (req, res) {
+    res.send('toto')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
